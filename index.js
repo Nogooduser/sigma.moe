d96df3aea5f7a8132cb37e487bb7eabb1f6ba52c
Nogooduser <nice.bakuretsu@gmail.com>
2021-02-05 18:25:11 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ if ((width >= 1000)){
             $(this).css("border-bottom","2px solid transparent");
         });
     }else{
-        $("#header").css("background","transparent");
+        $("#header").css("background","#121212");
         $("#header").css("color","#fff");
         $("#header").css("box-shadow","0px 0px 0px rgba(0,0,0,0)");
-        $("#header").css("padding","6vh 4vw");
+        $("#header").css("padding","0vh 4vw");
         $("#navigation a").hover(function(){
             $(this).css("border-bottom","2px solid #fff");
         },function(){
